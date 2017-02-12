6503ab1b28c3f4a5623c4e2e83b266886a8c0a1f
Artem Cherebaev <geronious@mail.ru>
2017-02-13 02:03:34 +0300

    Small fixes (due to refactoring).

--- liferay-plugins-sdk-6.2/portlets/tiis-library-portlet/docroot/WEB-INF/src/ru/tiis/constants/PortletKeys.java
+++ liferay-plugins-sdk-6.2/portlets/tiis-library-portlet/docroot/WEB-INF/src/ru/tiis/constants/PortletKeys.java
@@ -2,10 +2,10 @@ package ru.tiis.constants;
 
 public class PortletKeys extends com.liferay.portal.util.PortletKeys {
 
-	public static final String LIBRARY_MANAGEMENT = "library-managment_WAR_tiis-library-portlet";
+	public static final String LIBRARY_MANAGEMENT = "library-managment_WAR_tiislibraryportlet";
 	
-	public static final String LIBRARY = "library_WAR_tiis-library-portlet";
+	public static final String LIBRARY = "library_WAR_tiislibraryportlet";
 	
-	public static final String BOOK_INFO = "book-info_WAR_tiis-library-portlet";
+	public static final String BOOK_INFO = "book-info_WAR_tiislibraryportlet";
 	
 }
